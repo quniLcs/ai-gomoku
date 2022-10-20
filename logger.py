8ceab9af4ebd9dd212692cad728690e183b04293
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-10-20 21:30:24 +0800

    Add files via upload

--- logger.py
+++ logger.py
@@ -2,19 +2,19 @@ import traceback
 
 
 class Logger:
-	def __init__(self):
-		self.path = 'D:/AI/ProjectFinal/template/pbrain-alphabeta.log'
-		with open(self.path, 'w') as file:
-			pass
+    def __init__(self):
+        self.path = 'D:/AI/ProjectFinal/mine/pbrain-alphabeta.log'
+        with open(self.path, 'w') as file:
+            pass
 
-	def log(self, text):
-		with open(self.path, 'a') as file:
-			file.write(text)
-			file.write('\n')
-			file.flush()
+    def log(self, text):
+        with open(self.path, 'a') as file:
+            file.write(text)
+            file.write('\n')
+            file.flush()
 
-	def crash(self):
-		with open(self.path, 'a') as file:
-			traceback.print_exc(file = file)
-			file.flush()
-		raise Exception
+    def crash(self):
+        with open(self.path, 'a') as file:
+            traceback.print_exc(file = file)
+            file.flush()
+        raise Exception
---
+++ pbrain-alphabeta.py
@@ -0,0 +1,34 @@
+# Special thanks to Yiwei Ding as well as his GitHub repo
+# https://github.com/suncerock/AI_final_project
+
+# the command to build an executable file
+# pyinstaller pbrain-alphabeta.py pisqpipe.py --name pbrain-alphabeta.exe --onefile
+
+
+import pisqpipe as pp
+# from pisqpipe import DEBUG_EVAL, DEBUG
+
+from logger import Logger
+from board import AlphaBetaBoard
+
+
+if __name__ == '__main__':
+	logger = Logger()
+	board = AlphaBetaBoard()
+
+	pp.infotext = "pbrain-alphabeta, Wang Yiqun"
+	pp.brain_init = board.init
+	pp.brain_restart = board.restart
+	pp.brain_turn = board.turn
+	pp.brain_my = board.my
+	pp.brain_opponents = board.opponents
+	pp.brain_block = board.block
+	pp.brain_takeback = board.takeback
+	pp.brain_end = board.end
+	pp.brain_eval = board.eval
+	pp.brain_about = board.about
+
+	try:
+		pp.main()
+	except:
+		logger.crash()
